837479f5ec26abc05ea5cd1e107d1d5a78451fab
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-03-15 21:59:23 +0300

    add snowman ver 1

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/t
 import {GUI} from 'https://threejsfundamentals.org/threejs/../3rdparty/dat.gui.module.js';
 
 let camera;
+let scene;
+const parameters =
+    {
+      fill: true, // boolean (checkbox)
+      rotation: 'No',
+      w: '...', // dummy value, only type is important
+      x: 0, y: 0, z: 0,
+    };
+
+
+const objects = [];
 
 
 function setUpCamera() {
@@ -14,41 +25,21 @@ function setUpCamera() {
   camera.position.set(0, 10, 20);
 }
 
-class MinMaxGUIHelper {
-  constructor(obj, minProp, maxProp, minDif) {
-    this.obj = obj;
-    this.minProp = minProp;
-    this.maxProp = maxProp;
-    this.minDif = minDif;
-  }
 
-  get min() {
-    return this.obj[this.minProp];
-  }
-
-  set min(v) {
-    this.obj[this.minProp] = v;
-    this.obj[this.maxProp] = Math.max(this.obj[this.maxProp], v + this.minDif);
-  }
-
-  get max() {
-    return this.obj[this.maxProp];
-  }
-
-  set max(v) {
-    this.obj[this.maxProp] = v;
-    this.min = this.min; // this will call the min setter
-  }
-}
-
-function createCube(scene) {
+function createCube(isFilled) {
+  clearScene();
+  let mesh;
   const cubeSize = 4;
   const cubeGeo = new THREE.BoxBufferGeometry(cubeSize, cubeSize, cubeSize);
   const cubeMat = new THREE.MeshPhongMaterial({color: '#8AC'});
-  const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+  if (isFilled) {
+    mesh = new THREE.Mesh(cubeGeo, cubeMat);
+  } else mesh = new THREE.LineSegments(cubeGeo, cubeMat);
   mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
-  scene.add(mesh);
+  objects[0] = mesh;
+  scene.add(objects[0]);
 }
+
 
 function setUpTextureLoader(scene) {
   const planeSize = 40;
@@ -71,29 +62,129 @@ function setUpTextureLoader(scene) {
   scene.add(mesh);
 }
 
+function clearScene() {
+  scene.remove(objects[0]);
+}
 
-function createSphere(scene) {
+
+function createSphere(isFilled) {
+  let mesh;
+  clearScene();
   const sphereRadius = 3;
   const sphereWidthDivisions = 32;
   const sphereHeightDivisions = 16;
   const sphereGeo = new THREE.SphereBufferGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
   const sphereMat = new THREE.MeshPhongMaterial({color: '#CA8'});
-  const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+  if (isFilled) {
+    mesh = new THREE.Mesh(sphereGeo, sphereMat);
+  } else mesh = new THREE.LineSegments(sphereGeo, sphereMat);
   mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
-  scene.add(mesh);
+  objects[0] = mesh;
+  scene.add(objects[0]);
 }
 
+function createSnowman(isFilled) {
+  clearScene();
+  let item;
+  const group = new THREE.Group();
 
-function updateCamera() {
-  camera.updateProjectionMatrix();
+
+  let itemGeometry = new THREE.SphereGeometry(4.5, 10);
+  let wireframe = new THREE.MeshPhongMaterial({color: '#CA8'});
+  if (isFilled) {
+    item = new THREE.Mesh(itemGeometry, wireframe);
+  } else {
+    item = new THREE.LineSegments(wireframe);
+  }
+  group.add(item);
+
+  itemGeometry = new THREE.SphereGeometry(3, 10);
+  wireframe = new THREE.MeshPhongMaterial({color: '#CA8'});
+  if (isFilled) {
+    item = new THREE.Mesh(itemGeometry, wireframe);
+  } else {
+    item = new THREE.LineSegments(wireframe);
+  }
+  item.position.y = 6;
+  group.add(item);
+
+  itemGeometry = new THREE.SphereGeometry(1.5, 10);
+  wireframe = new THREE.MeshPhongMaterial({color: '#CA8'});
+  if (isFilled) {
+    item = new THREE.Mesh(itemGeometry, wireframe);
+  } else {
+    item = new THREE.LineSegments(wireframe);
+  }
+  item.position.y = 5.5 + 4.3;
+  group.add(item);
+
+  itemGeometry = new THREE.SphereGeometry(0.2, 5);
+  wireframe = new THREE.MeshPhongMaterial({color: '#CA8'});
+  if (isFilled) {
+    item = new THREE.Mesh(itemGeometry, wireframe);
+  } else {
+    item = new THREE.LineSegments(wireframe);
+  }
+  item.position.y = 5.5 + 4.3 + 0.5;
+  item.position.z = 1.3;
+  group.add(item);
+
+  itemGeometry = new THREE.SphereGeometry(2, 50);
+  wireframe = new THREE.MeshPhongMaterial({color: '#CA8'});
+  if (isFilled) {
+    item = new THREE.Mesh(itemGeometry, wireframe);
+  } else {
+    item = new THREE.LineSegments(wireframe);
+  }
+  item.position.x = -0.7;
+  item.position.y = 5.5 + 4.3 + 0.5;
+  item.position.z = 1;
+  group.add(item);
+  objects[0] = group;
+  scene.add(objects[0]);
 }
+
 
 function setUpGui() {
   const gui = new GUI();
-  gui.add(camera, 'fov', 1, 180).onChange(updateCamera);
-  const minMaxGUIHelper = new MinMaxGUIHelper(camera, 'near', 'far', 0.1); // отображает настройки
-  gui.add(minMaxGUIHelper, 'min', 0.1, 50, 0.1).name('near').onChange(updateCamera);
-  gui.add(minMaxGUIHelper, 'max', 0.1, 50, 0.1).name('far').onChange(updateCamera);
+  gui.add(parameters, 'fill').name('Залить?');
+  const rotationList = ['X', 'Y', 'Z', 'No'];
+  gui.add(parameters, 'rotation', rotationList).name('Вращение').onChange((v) => {
+    console.log(parameters.rotation);
+  });
+
+  function ListHandler(v) {
+    switch (v) {
+      case 'Cube':
+        createCube(parameters.fill);
+        break;
+      case 'Sphere':
+        createSphere(parameters.fill);
+        break;
+      case 'Snowman':
+        createSnowman(parameters.fill);
+        break;
+    }
+  }
+
+  const stringList = ['Sphere', 'Cube', 'Snowman'];
+  gui.add(parameters, 'w', stringList).name('Список фигур').onChange(ListHandler);
+
+  const folder1 = gui.addFolder('Coordinates');
+  folder1.add(parameters, 'x').onChange((xCoord) => {
+    // eslint-disable-next-line max-len
+    objects.forEach((obj) => obj.position.set(xCoord, obj.position.y, obj.position.z));
+  });
+  folder1.add(parameters, 'y').onChange((yCoord) => {
+    // eslint-disable-next-line max-len
+    objects.forEach((obj) => obj.position.set(obj.position.x, yCoord, obj.position.z));
+  });
+  folder1.add(parameters, 'z').onChange((zCoord) => {
+    // eslint-disable-next-line max-len
+    objects.forEach((obj) => obj.position.set(obj.position.x, obj.position.y, zCoord));
+  });
+  folder1.close();
+  gui.open();
 }
 
 
@@ -131,25 +222,45 @@ function resizeRendererToDisplaySize(renderer) {
   return needResize;
 }
 
+function updateRotation(obj, time) {
+  switch (parameters.rotation) {
+    case 'No':
+      break;
+    case 'X':
+      obj.rotation.x = time;
+      break;
+    case 'Y':
+      obj.rotation.y = time;
+      break;
+    case 'Z':
+      obj.rotation.z = time;
+      break;
+  }
+}
 
 function main() {
   setUpCamera();
   setUpGui();
   const renderer = setUpCanvas();
 
-  const scene = new THREE.Scene();
+  scene = new THREE.Scene();
   scene.background = new THREE.Color('black');
   setUpTextureLoader(scene);
   setUpLight(scene);
-  createSphere(scene);
-  createCube(scene);
 
-  const render = () => {
+  const render = (time) => {
+    time *= 0.01; // перевод в секунды
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
     }
+
+    objects.forEach((obj) => {
+      if (obj != null) {
+        updateRotation(obj, time);
+      }
+    });
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   };
