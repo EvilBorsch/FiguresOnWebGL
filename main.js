d5958126541221fa18dbc9a53b04b01f61e7dd53
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-03-15 18:54:14 +0300

    => func render

--- main.js
+++ main.js
@@ -110,55 +110,49 @@ function setUpCanvas() {
 
 
 function setUpLight(scene) {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(0, 10, 0);
-    light.target.position.set(-5, 0, 0);
-    scene.add(light);
-    scene.add(light.target);
+  const color = 0xFFFFFF;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(0, 10, 0);
+  light.target.position.set(-5, 0, 0);
+  scene.add(light);
+  scene.add(light.target);
 }
 
 
-
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
 
 
 function main() {
   setUpCamera();
   setUpGui();
-  const renderer=setUpCanvas();
+  const renderer = setUpCanvas();
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color('black');
   setUpTextureLoader(scene);
-  setUpLight(scene)
+  setUpLight(scene);
   createSphere(scene);
   createCube(scene);
 
-
-  function resizeRendererToDisplaySize(renderer) {
-    const canvas = renderer.domElement;
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
-    const needResize = canvas.width !== width || canvas.height !== height;
-    if (needResize) {
-      renderer.setSize(width, height, false);
-    }
-    return needResize;
-  }
-
-  function render() {
+  const render = () => {
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
     }
-
     renderer.render(scene, camera);
-
     requestAnimationFrame(render);
-  }
-
+  };
   requestAnimationFrame(render);
 }
 
